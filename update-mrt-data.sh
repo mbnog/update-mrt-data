3a7bcb63e1c603c0a3383a3b55bca7ce64f5b2f3
Theodore Baschak <theodore@ciscodude.net>
2020-04-11 17:49:06 -0500

    Updated Stages to print after, and use local path for mrt2mysql

--- update-mrt-data.sh
+++ update-mrt-data.sh
@@ -47,20 +47,20 @@ wait
 
 cat > task1.sh <<-__EOF__
   #!/bin/sh
-  echo "Stage 1: $1";
   bgpscanner -p "$1\$" rib.20200410.2016 | grep -v 16395:9:0 > routes/$1.routes.txt
+  echo "Stage 1: $1";
   __EOF__
   
 cat > task2.sh <<-__EOF__
   #!/bin/sh
-  echo "Stage 2: $1";
   bgpscanner -p "$1\$" latest-bview >> routes/$1.routes.txt
+  echo "Stage 2: $1";
   __EOF__
 
 cat > task3.sh <<-__EOF__
   #!/bin/sh
+  mrt2mysql/mrt2mysql-batchedcommit.py < $1
   echo "Stage 3: $1";
-  ~/dev/theo/mrt2mysql/mrt2mysql-batchedcommit.py <  $1
   __EOF__
 
 # do the processing in parallel as much as possible (within the limits of shell scripting)
